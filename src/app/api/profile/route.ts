722c64c62f490cf034b28732b2819e6499fbbb3b
sho <supereagle25@gmail.com>
2025-08-12 15:47:13 +0900

    Refactor: Move authOptions to lib and update imports

--- src/app/api/profile/route.ts
+++ src/app/api/profile/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 import prisma from '@/lib/prisma';
 import { getServerSession } from 'next-auth/next';
-import { authOptions } from '@/app/api/auth/[...nextauth]/route';
+import { authOptions } from '@/lib/authOptions';
 
 export async function PUT(request: Request) {
     const session = await getServerSession(authOptions);
